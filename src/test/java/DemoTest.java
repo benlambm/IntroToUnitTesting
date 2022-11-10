dabfd54aaede325da054590f7ca50e1d91f24f13
Ben Lamb <benlamb@vt.edu>
2022-11-10 16:23:55 -0500

    Create DemoTest and initial unit tests

--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -1,3 +1,7 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
 /**
  * The class containing your tests for the {@link Demo} class.  Make sure you
  * test all methods in this class (including both 
@@ -5,5 +9,20 @@
  * {@link Demo#isTriangle(double, double, double)}).
  */
 public class DemoTest {
+
+
+    @Test
+    public void test_is_triangle_1() {
+        assertTrue(Demo.isTriangle(3,4,5));
+    }
+    @Test
+    public void test_is_triangle_2() {
+        assertTrue(Demo.isTriangle(3,3,3));
+    }
+    @Test
+    public void test_is_triangle_3() {
+        assertTrue(Demo.isTriangle(5,12,13));
+    }
+
 
 }
